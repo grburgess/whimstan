d080638c6f6e24a62afc92a6516763dfcfd7c951
grburgess <jmichaelburgess@gmail.com>
2021-02-19 18:21:32 +0100

    updates

--- whimstan/__init__.py
+++ whimstan/__init__.py
@@ -1,5 +1,6 @@
-from .prep import build_stan_data, XRTCatalog
-
 from ._version import get_versions
+from .prep import XRTCatalog, build_stan_data
+from .stan_code.stan_models import get_model
+
 __version__ = get_versions()['version']
 del get_versions
