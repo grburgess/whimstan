f116121eca46569d84b30746556464aaf9df5a80
grburgess <jmichaelburgess@gmail.com>
2021-02-24 23:46:38 +0100

    added very important science things

--- whimstan/__init__.py
+++ whimstan/__init__.py
@@ -1,6 +1,11 @@
+from IPython.display import YouTubeVideo, display
+
 from ._version import get_versions
 from .prep import XRTCatalog, build_stan_data
 from .stan_code.stan_models import get_model
+
+display(YouTubeVideo("Tb6tz6ohprw", start=17, autoplay=1))
+
 
 __version__ = get_versions()['version']
 del get_versions
