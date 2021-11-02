aa5b933b42fba069348caa66d172b8466f9caae6
grburgess <jmichaelburgess@gmail.com>
2021-11-02 10:05:24 +0100

    this is the way

--- whimstan/population_generator.py
+++ whimstan/population_generator.py
@@ -4,7 +4,7 @@ from typing import Dict, List, Optional
 import astropy.units as u
 import numba as nb
 import numpy as np
-import popsynth
+import popsynth as ps
 import scipy.special as sf
 from astromodels import Powerlaw_Eflux, TbAbs
 from astropy.coordinates import SkyCoord
@@ -15,16 +15,16 @@ from scipy.integrate import quad
 
 
 class Cloud:
-    def __init__(self, R: float=1, zr_ratio:float=1.):
+    def __init__(self, R: float = 1, zr_ratio: float = 1.0):
         """
         Creates an ellipsoidal cloud from which a
         GRB's emission would have to propagate
-        
-        :param R: 
-        :type R: 
-        :param zr_ratio: 
-        :type zr_ratio: 
-        :returns: 
+
+        :param R:
+        :type R:
+        :param zr_ratio:
+        :type zr_ratio:
+        :returns:
 
         """
         self._R = R
@@ -33,14 +33,14 @@ class Cloud:
         self._Z = self._R * self._zr_ratio
         self._Z2 = self._Z * self._Z
         self._size_vec = np.array([self._R, self._R, self._Z])
-        self._size_vec2 = self._size_vec**2
+        self._size_vec2 = self._size_vec ** 2
 
     def sample(self) -> float:
         """
         generate a random point inside the cloud
         and return its path length to the observer
 
-        :returns: 
+        :returns:
 
         """
         p = self.generate_point_inside()
@@ -56,16 +56,20 @@ class Cloud:
         generate a random point inside the cloud
         via rejection sampling
 
-        :returns: 
+        :returns:
 
         """
         flag = True
         while flag:
-            p = np.array([np.random.uniform(-self._R, self._R),
-                          np.random.uniform(-self._R, self._R),
-                          np.random.uniform(-self._Z, self._Z)])
+            p = np.array(
+                [
+                    np.random.uniform(-self._R, self._R),
+                    np.random.uniform(-self._R, self._R),
+                    np.random.uniform(-self._Z, self._Z),
+                ]
+            )
 
-            test_val = (p**2).dot(1./self._size_vec2)
+            test_val = (p ** 2).dot(1.0 / self._size_vec2)
 
             if test_val <= 1:
 
@@ -77,7 +81,7 @@ class Cloud:
         """
         generate a random unit vector
 
-        :returns: 
+        :returns:
 
         """
         v = np.random.normal(size=3)
@@ -88,29 +92,29 @@ class Cloud:
     def compute_path_length(self, p, u) -> float:
         """
         compute the path length between two vectors
-        
-        :param p: 
-        :type p: 
-        :param u: 
-        :type u: 
-        :returns: 
+
+        :param p:
+        :type p:
+        :param u:
+        :type u:
+        :returns:
 
         """
-        b = 2 * (p * u).dot(1./self._size_vec2)
-        a = (u**2).dot(1./self._size_vec2)
-        c = (p**2).dot(1./self._size_vec2)
+        b = 2 * (p * u).dot(1.0 / self._size_vec2)
+        a = (u ** 2).dot(1.0 / self._size_vec2)
+        c = (p ** 2).dot(1.0 / self._size_vec2)
 
-        l = (-b + np.sqrt(b*b - 4 * a * (c-1)))/(2*a)
+        l = (-b + np.sqrt(b * b - 4 * a * (c - 1))) / (2 * a)
 
         return l
 
 
-class SchechterSampler(popsynth.AuxiliarySampler):
+class SchechterSampler(ps.AuxiliarySampler):
 
     _auxiliary_sampler_name = "SchechterSampler"
 
-    Lmin = popsynth.auxiliary_sampler.AuxiliaryParameter(default=1, vmin=0)
-    alpha = popsynth.auxiliary_sampler.AuxiliaryParameter(default=1)
+    Lmin = ps.auxiliary_sampler.AuxiliaryParameter(default=1, vmin=0)
+    alpha = ps.auxiliary_sampler.AuxiliaryParameter(default=1)
 
     def __init__(self, name="plaw_flux"):
         """
@@ -120,8 +124,7 @@ class SchechterSampler(popsynth.AuxiliarySampler):
         :rtype:
         """
 
-        super(SchechterSampler, self).__init__(name=name,
-                                               observed=False)
+        super(SchechterSampler, self).__init__(name=name, observed=False)
 
     def true_sampler(self, size):
         """FIXME! briefly describe function
@@ -134,11 +137,10 @@ class SchechterSampler(popsynth.AuxiliarySampler):
         self._true_values = self.Lmin * sf.gammaincinv(1 + self.alpha, xs)
 
 
-class HostGas(popsynth.AuxiliarySampler):
+class HostGas(ps.AuxiliarySampler):
     _auxiliary_sampler_name = "HostGas"
-    nh_mean = popsynth.auxiliary_sampler.AuxiliaryParameter(default=22, vmin=0)
-    zratio = popsynth.auxiliary_sampler.AuxiliaryParameter(
-        default=1, vmin=0, vmax=1)
+    nh_mean = ps.auxiliary_sampler.AuxiliaryParameter(default=22, vmin=0)
+    zratio = ps.auxiliary_sampler.AuxiliaryParameter(default=1, vmin=0, vmax=1)
 
     def __init__(self):
         """
@@ -158,10 +160,10 @@ class HostGas(popsynth.AuxiliarySampler):
 
         path_length = np.array([cloud.sample() for i in range(size)])
 
-        self._true_values = np.power(10., self.nh_mean) * path_length
+        self._true_values = np.power(10.0, self.nh_mean) * path_length
 
 
-class MilkyWayGas(popsynth.AuxiliarySampler):
+class MilkyWayGas(ps.AuxiliarySampler):
     _auxiliary_sampler_name = "MilkyWayGas"
 
     def __init__(self):
@@ -172,7 +174,8 @@ class MilkyWayGas(popsynth.AuxiliarySampler):
         # pass up to the super class
 
         super(MilkyWayGas, self).__init__(
-            "mw_nh", observed=False, uses_sky_position=True)
+            "mw_nh", observed=False, uses_sky_position=True
+        )
 
     def true_sampler(self, size):
 
@@ -180,23 +183,22 @@ class MilkyWayGas(popsynth.AuxiliarySampler):
 
         coord = SkyCoord(ra=self._ra, dec=self._dec, unit="deg", frame="icrs")
 
-        for i,c in enumerate(coord):
+        for i, c in enumerate(coord):
 
-            mw_nh[i] = GasMap.nhf(c, nhmap='DL', radius=1*u.deg).value
+            mw_nh[i] = GasMap.nhf(c, nhmap="DL", radius=1 * u.deg).value
 
         self._true_values = mw_nh
 
 
-class MWGasSelection(popsynth.SelectionProbabilty):
+class MWGasSelection(ps.SelectionProbability):
 
     _selection_name = "MWGasSelection"
 
-    gas_limit = popsynth.SelectionParameter(
-        vmin=0)
+    gas_limit = ps.SelectionParameter(vmin=0)
 
     def __init__(self, name="mw gas selector"):
         """
-        places a limit on the amount of MW gas allowed for 
+        places a limit on the amount of MW gas allowed for
         each object in the sample
 
         """
@@ -205,14 +207,14 @@ class MWGasSelection(popsynth.SelectionProbabilty):
 
     def draw(self, size: int):
 
-        self._selection = self._observed_value < np.power(10., self.gas_limit)
+        self._selection = self._observed_value < np.power(10.0, self.gas_limit)
 
 
-class GalacticPlaceSelection(popsynth.SpatialSelection):
+class GalacticPlaceSelection(ps.SpatialSelection):
 
     _selection_name = "GalacticPlaceSelection"
 
-    b_limit = popsynth.SelectionParameter(vmin=0, vmax=90)
+    b_limit = ps.SelectionParameter(vmin=0, vmax=90)
 
     def __init__(self, name="mw plane selector"):
         """
@@ -222,20 +224,31 @@ class GalacticPlaceSelection(popsynth.SpatialSelection):
 
     def draw(self, size: int):
 
-        g_coor = SkyCoord(self._spatial_distribution.ra, self._spatial_distribution.dec, unit="deg",
-                          frame="icrs").transform_to("galactic")
+        g_coor = SkyCoord(
+            self._spatial_distribution.ra,
+            self._spatial_distribution.dec,
+            unit="deg",
+            frame="icrs",
+        ).transform_to("galactic")
 
         self._selection = (g_coor.b.deg >= self.b_limit) | (
-            g_coor.b.deg <= -self.b_limit)
+            g_coor.b.deg <= -self.b_limit
+        )
 
 
-class ObscuredFluxSampler(popsynth.DerivedLumAuxSampler):
+class ObscuredFluxSampler(ps.DerivedLumAuxSampler):
 
     _auxiliary_sampler_name = "ObscuredFluxSampler"
 
-    def __init__(self, a: float = .4, b: float = 15,
-                 whim_n0: Optional[float] = None, whim_T: Optional[float] = None,
-                 use_mw_gas: bool = True, use_host_gas: bool = True):
+    def __init__(
+        self,
+        a: float = 0.4,
+        b: float = 15,
+        whim_n0: Optional[float] = None,
+        whim_T: Optional[float] = None,
+        use_mw_gas: bool = True,
+        use_host_gas: bool = True,
+    ):
         """
         computes the obscured flux from the GRB by integrating the spectra
 
@@ -256,7 +269,8 @@ class ObscuredFluxSampler(popsynth.DerivedLumAuxSampler):
         self._use_host_gas = use_host_gas
 
         super(ObscuredFluxSampler, self).__init__(
-            "obscured_flux", uses_distance=True)
+            "obscured_flux", uses_distance=True
+        )
 
     def true_sampler(self, size):
 
@@ -266,7 +280,8 @@ class ObscuredFluxSampler(popsynth.DerivedLumAuxSampler):
         n_energies_for_intergration = 50
 
         intergration_energies = np.geomspace(
-            self._a, self._b, num=n_energies_for_intergration)
+            self._a, self._b, num=n_energies_for_intergration
+        )
 
         out = np.empty(size)
 
@@ -275,34 +290,48 @@ class ObscuredFluxSampler(popsynth.DerivedLumAuxSampler):
 
         for i in progress_bar(range(size), desc="computing obscured fluxes"):
 
-            spec = Powerlaw_Eflux(F=self._secondary_samplers["plaw_flux"].true_values[i]/(4 * np.pi * (self.luminosity_distance[i]**2)),
-                                  index=self._secondary_samplers["spec_idx"].true_values[i],
-                                  a=self._a,
-                                  b=self._b)
+            spec = Powerlaw_Eflux(
+                F=self._secondary_samplers["plaw_flux"].true_values[i]
+                / (4 * np.pi * (self.luminosity_distance[i] ** 2)),
+                index=self._secondary_samplers["spec_idx"].true_values[i],
+                a=self._a,
+                b=self._b,
+            )
             if self._use_mw_gas:
                 spec *= TbAbs(
-                    NH=self._secondary_samplers["mw_nh"].true_values[i]/(1.e22), redshift=0)
+                    NH=self._secondary_samplers["mw_nh"].true_values[i]
+                    / (1.0e22),
+                    redshift=0,
+                )
             if self._use_host_gas:
-                spec *= TbAbs(NH=self._secondary_samplers["host_nh"].true_values[i]/(
-                    1.e22), redshift=self._distance[i])
+                spec *= TbAbs(
+                    NH=self._secondary_samplers["host_nh"].true_values[i]
+                    / (1.0e22),
+                    redshift=self._distance[i],
+                )
 
             # add on the WHIM if needed
             if (self._whim_n0 is not None) and (self._whim_T is not None):
 
-                spec = spec * \
-                    Integrate_Absori(n0=self._whim_n0,
-                                     temp=self._whim_T,
-                                     redshift=self._distance[i])
+                spec = spec * Integrate_Absori(
+                    n0=self._whim_n0,
+                    temp=self._whim_T,
+                    redshift=self._distance[i],
+                )
 
             # now compute the energy integral.
             # using the slower quad here because
             # the gas models have a shit load of lines
 
-            #flux = quad(lambda x: x * spec(x), self._a, self._b)[0] * kev2erg
+            # flux = quad(lambda x: x * spec(x), self._a, self._b)[0] * kev2erg
 
-            flux = np.trapz(
-                intergration_energies*spec(intergration_energies),
-                intergration_energies,) * kev2erg
+            flux = (
+                np.trapz(
+                    intergration_energies * spec(intergration_energies),
+                    intergration_energies,
+                )
+                * kev2erg
+            )
 
             out[i] = flux
 
@@ -312,29 +341,30 @@ class ObscuredFluxSampler(popsynth.DerivedLumAuxSampler):
 
         # have to compute back to a luminosity
 
-        return (4.0 * np.pi * self.luminosity_distance**2) * self._true_values
+        return (4.0 * np.pi * self.luminosity_distance ** 2) * self._true_values
 
 
-def create_simulation(r0: float = 5,
-                      a: float = 0.0157,
-                      rise: float = .118,
-                      decay: float = 4.2,
-                      peak: float = 3.4,
-                      z_max: float = 10.,
-                      Lmin: float = 1e46,
-                      alpha: float = 1.5,
-                      host_gas_mean: float = 23,
-                      host_gas_cloud_ratio: float = 0.1,
-                      mw_nh_limit: Optional[float] = None,
-                      b_limit: Optional[float] = None,
-                      use_clouds: bool = True,
-                      spec_idx_mean: float = -2.,
-                      spec_idx_std: float = .2,
-                      use_mw_gas: bool = True,
-                      use_host_gas: bool = True,
-                      whim_n0: Optional[float] = None,
-                      whim_T: Optional[float] = None
-                      ) -> popsynth.PopulationSynth:
+def create_simulation(
+    r0: float = 5,
+    a: float = 0.0157,
+    rise: float = 0.118,
+    decay: float = 4.2,
+    peak: float = 3.4,
+    z_max: float = 10.0,
+    Lmin: float = 1e46,
+    alpha: float = 1.5,
+    host_gas_mean: float = 23,
+    host_gas_cloud_ratio: float = 0.1,
+    mw_nh_limit: Optional[float] = None,
+    b_limit: Optional[float] = None,
+    use_clouds: bool = True,
+    spec_idx_mean: float = -2.0,
+    spec_idx_std: float = 0.2,
+    use_mw_gas: bool = True,
+    use_host_gas: bool = True,
+    whim_n0: Optional[float] = None,
+    whim_T: Optional[float] = None,
+) -> ps.PopulationSynth:
 
     if use_host_gas:
         if use_clouds:
@@ -352,8 +382,9 @@ def create_simulation(r0: float = 5,
             # the host gas will be drawn from
             # a log normal which is empirical
 
-            host_nh = popsynth.aux_samplers.Log10NormalAuxSampler(
-                name="host_nh", observed=False)
+            host_nh = ps.aux_samplers.Log10NormalAuxSampler(
+                name="host_nh", observed=False
+            )
             host_nh.mu = host_gas_mean
             host_nh.sigma = 0.5
 
@@ -374,8 +405,7 @@ def create_simulation(r0: float = 5,
     # sample the spectral index
     # of the power law
 
-    spec_idx = popsynth.aux_samplers.NormalAuxSampler(
-        name="spec_idx", observed=False)
+    spec_idx = ps.aux_samplers.NormalAuxSampler(name="spec_idx", observed=False)
     spec_idx.mu = spec_idx_mean
     spec_idx.sigma = spec_idx_std
 
@@ -389,8 +419,12 @@ def create_simulation(r0: float = 5,
     # that would lead to the flux actually observed
     # by the XRT
 
-    ls = ObscuredFluxSampler(whim_n0=whim_n0, whim_T=whim_T,
-                             use_mw_gas=use_mw_gas, use_host_gas=use_host_gas)
+    ls = ObscuredFluxSampler(
+        whim_n0=whim_n0,
+        whim_T=whim_T,
+        use_mw_gas=use_mw_gas,
+        use_host_gas=use_host_gas,
+    )
 
     ls.set_secondary_sampler(spec_idx)
     ls.set_secondary_sampler(powerlaw)
@@ -399,16 +433,13 @@ def create_simulation(r0: float = 5,
     if use_mw_gas:
         ls.set_secondary_sampler(mw_nh)
 
-    pop_gen: popsynth.PopulationSynth = popsynth.populations.SFRPopulation(
+    pop_gen: ps.PopulationSynth = ps.populations.SFRPopulation(
         r0=r0,
         a=a,
         rise=rise,
         decay=decay,
         peak=peak,
         r_max=z_max,
-
-
-
     )
 
     pop_gen.add_observed_quantity(ls)
--- whimstan/simulation.py
+++ whimstan/simulation.py
@@ -1,4 +1,3 @@
-
 from pathlib import Path
 from typing import Dict, List, Optional
 
@@ -15,38 +14,51 @@ from .catalog import XRTCatalog, XRTCatalogEntry
 
 
 class SpectrumGenerator:
-
-    def __init__(self, name: str, eflux: float, index: float, ra: float, dec: float, z: float, host_nh: float, mw_nh: float, whim_n0: Optional[float] = None, whim_T: Optional[float] = None, demo_plugin: Optional[OGIPLike] = None,
-                 use_mw_gas: bool = True, use_host_gas: bool = True):
+    def __init__(
+        self,
+        name: str,
+        eflux: float,
+        index: float,
+        ra: float,
+        dec: float,
+        z: float,
+        host_nh: float,
+        mw_nh: float,
+        whim_n0: Optional[float] = None,
+        whim_T: Optional[float] = None,
+        demo_plugin: Optional[OGIPLike] = None,
+        use_mw_gas: bool = True,
+        use_host_gas: bool = True,
+    ):
         """
 
         :param name:  the name of the function
         :type name: str
-        :param eflux: 
+        :param eflux:
         :type eflux: float
-        :param index: 
+        :param index:
         :type index: float
-        :param ra: 
+        :param ra:
         :type ra: float
-        :param dec: 
+        :param dec:
         :type dec: float
-        :param z: 
+        :param z:
         :type z: float
         :param host_nh: host nH density in 1/10^22 cm2
         :type host_nh: float
         :param mw_nh: milkyway nH density in 1/10^22 cm2
         :type mw_nh: float
-        :param whim_n0: 
+        :param whim_n0:
         :type whim_n0: Optional[float]
-        :param whim_T: 
+        :param whim_T:
         :type whim_T: Optional[float]
-        :param demo_plugin: 
+        :param demo_plugin:
         :type demo_plugin: Optional[OGIPLike]
-        :param use_mw_gas: 
+        :param use_mw_gas:
         :type use_mw_gas: bool
-        :param use_host_gas: 
+        :param use_host_gas:
         :type use_host_gas: bool
-        :returns: 
+        :returns:
 
         """
 
@@ -66,7 +78,7 @@ class SpectrumGenerator:
         self._demo_plugin: Optional[OGIPLike] = demo_plugin
 
         # now this is done in pop synth
-#        self._get_mw_nh()
+        #        self._get_mw_nh()
 
         self._create_plugin()
 
@@ -74,24 +86,25 @@ class SpectrumGenerator:
 
         coord = SkyCoord(ra=self._ra, dec=self._dec, unit="deg", frame="icrs")
 
-        self._mw_nh = GasMap.nhf(
-            coord, nhmap='DL', radius=1*u.deg).value / 1e22
+        self._mw_nh = (
+            GasMap.nhf(coord, nhmap="DL", radius=1 * u.deg).value / 1e22
+        )
 
     def _create_plugin(self):
         quiet_mode()
 
         if self._demo_plugin is None:
 
-            self._demo_plugin = OGIPLike("tmp",
-                                         observation="data/grb050401/apcsource.pi",
-                                         background="data/grb050401/apcback.pi",
-                                         response="data/grb050401/apc.rmf",
-                                         arf_file="data/grb050401/apc.arf",
-                                         verbose=False
+            self._demo_plugin = OGIPLike(
+                "tmp",
+                observation="data/grb050401/apcsource.pi",
+                background="data/grb050401/apcback.pi",
+                response="data/grb050401/apc.rmf",
+                arf_file="data/grb050401/apc.arf",
+                verbose=False,
+            )
 
-                                         )
-
-        spec = Powerlaw_Eflux(F=self._eflux, index=self._index, a=.4, b=15)
+        spec = Powerlaw_Eflux(F=self._eflux, index=self._index, a=0.4, b=15)
         if self._use_mw_gas:
             spec *= TbAbs(NH=self._mw_nh, redshift=0)
         if self._use_host_gas:
@@ -99,9 +112,9 @@ class SpectrumGenerator:
 
         if (self._whim_n0 is not None) and (self._whim_T is not None):
 
-            spec = spec * \
-                Integrate_Absori(n0=self._whim_n0,
-                                 temp=self._whim_T, redshift=self._z)
+            spec = spec * Integrate_Absori(
+                n0=self._whim_n0, temp=self._whim_T, redshift=self._z
+            )
 
         ps = PointSource("tmp", self._ra, self._dec, spectral_shape=spec)
         model = Model(ps)
@@ -123,54 +136,63 @@ class SpectrumGenerator:
     @property
     def xrt_catalog_entry(self):
 
-        return XRTCatalogEntry(self._name.replace("grb", ""),
-                               self._ra,
-                               self._dec,
-                               self._z,
-                               self._mw_nh,
-                               nH_host_sim=self._host_nh,
-                               index_sim=self._index,
-                               flux_sim=self._eflux,
-                               n0_sim=self._whim_n0,
-                               temp_sim=self._whim_T
-
-
-                               )
+        return XRTCatalogEntry(
+            self._name.replace("grb", ""),
+            self._ra,
+            self._dec,
+            self._z,
+            self._mw_nh,
+            nH_host_sim=self._host_nh,
+            index_sim=self._index,
+            flux_sim=self._eflux,
+            n0_sim=self._whim_n0,
+            temp_sim=self._whim_T,
+        )
 
 
 class SpectrumFactory:
-
-    def __init__(self, population: popsynth.Population, whim_n0=None, whim_T=None,
-                 use_mw_gas=True, use_host_gas=True):
+    def __init__(
+        self,
+        population: popsynth.Population,
+        whim_n0=None,
+        whim_T=None,
+        use_mw_gas=True,
+        use_host_gas=True,
+    ):
 
         self._spectra = []
 
-        for i in progress_bar(range(population.n_objects), desc="Calculating the simulated datasets"):
+        for i in progress_bar(
+            range(population.n_objects),
+            desc="Calculating the simulated datasets",
+        ):
 
             name = f"grb00{i}"
 
             if use_mw_gas:
-                mw_nh = population.mw_nh[i]/1.e22
+                mw_nh = population.mw_nh[i] / 1.0e22
             else:
                 mw_nh = None
 
             if use_host_gas:
-                host_nh = population.host_nh[i]/1.e22
+                host_nh = population.host_nh[i] / 1.0e22
             else:
                 host_nh = None
 
-            sg = SpectrumGenerator(name=name,
-                                   eflux=population.fluxes_latent[i],
-                                   index=population.spec_idx[i],
-                                   ra=population.ra[i],
-                                   dec=population.dec[i],
-                                   z=population.distances[i],
-                                   host_nh=host_nh,
-                                   mw_nh=mw_nh,
-                                   whim_n0=whim_n0,
-                                   whim_T=whim_T,
-                                   use_mw_gas=use_mw_gas,
-                                   use_host_gas=use_host_gas)
+            sg = SpectrumGenerator(
+                name=name,
+                eflux=population.fluxes_latent[i],
+                index=population.spec_idx[i],
+                ra=population.ra[i],
+                dec=population.dec[i],
+                z=population.distances[i],
+                host_nh=host_nh,
+                mw_nh=mw_nh,
+                whim_n0=whim_n0,
+                whim_T=whim_T,
+                use_mw_gas=use_mw_gas,
+                use_host_gas=use_host_gas,
+            )
 
             self._spectra.append(sg)
 
