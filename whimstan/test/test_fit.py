8a4bccecd8056f3a0f1fad8ba1e1bcb12b04c123
grburgess <jmichaelburgess@gmail.com>
2022-02-08 08:38:09 +0100

    switch to yaml setup for fitting

--- whimstan/test/test_fit.py
+++ whimstan/test/test_fit.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 
-from whimstan import Database, Fit, SpectrumFactory, make_fit
+from whimstan import Database, Fit, Fitter, SpectrumFactory
+from whimstan.utils import get_path_of_data_file
 
 
 def test_fit(tiny_population):
@@ -9,33 +10,18 @@ def test_fit(tiny_population):
 
     factory.create_database("tiny_db.h5")
 
-    db = Database.read("tiny_db.h5")
+    fitter = Fitter.from_file(get_path_of_data_file("no_whim_demo.yml"))
 
-    fit_params = dict(
-        seed=3214,
-        inits={
-            "log_nH_host_mu_raw": 0,
-            "log_nH_host_sigma": 0.5,
-            "log_K_mu_raw": -1,
-            "index_mu": -2.0,
-            "host_alpha": -2.0,
-        },
-        iter_warmup=1,
-        iter_sampling=5,
-        max_treedepth=9,
-    )
+    fitter.config.database = "tiny_db.h5"
+    fitter.config.fit_setup.fit_params.iter_warmup = 1
+    fitter.config.fit_setup.fit_params.iter_sampling = 5
+    fitter.config.fit_setup.fit_params.max_treedepth = 9
+    fitter.config.fit_setup.n_chains =1
+    fitter.config.fit_setup.n_threads = 1
 
-    make_fit(
-        "no_whim",
-        db,
-        n_threads=1,
-        fit_params=fit_params,
-        file_name="fit_no_whim.h5",
-        n_chains=1,
-        # use_opencl=True,
-        use_host_gas=True,
-        use_mw_gas=True,
-        use_absori=False,
+    fitter.config.file_name = "fit_no_whim.h5"
+
+    fitter.make_fit(
         clean_model=True,
     )
 
